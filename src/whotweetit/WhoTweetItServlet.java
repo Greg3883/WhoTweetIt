e2e7c5c19ed1d4aff37e677248a3e5989026cffa
tarcourt <theo.arcourt@etu.univ-nantes.fr>
2017-03-09 14:29:38 +0100

    lol

--- src/whotweetit/WhoTweetItServlet.java
+++ src/whotweetit/WhoTweetItServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.*;
 public class WhoTweetItServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().print("lol");
+		// The factory instance is re-useable and thread safe.
+	    //fdsfdsfdsfdsfsdfds
 	}
 }
